3869e9978e3f53edf87fbc2ac0d26c22910c5594
Darrel Muonekwu <darrelmuonekwu@gmail.com>
2019-05-21 13:32:09 -0700

    Finished the PandaExpress food section page

--- Roam/ViewControllers/PandaMealSelectionController.swift
+++ Roam/ViewControllers/PandaMealSelectionController.swift
@@ -8,23 +8,79 @@
 
 import UIKit
 
-class PandaMealSelectionController: UIViewController {
-
+class PandaMealSelectionController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if [0,2,4,6].contains(section) {
+            print("even section: \(section)")
+            return 1
+        }
+        return 2
     }
-    */
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 8
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let section  = indexPath.section
+        let row = indexPath.row
+        
+        if [0,2,4,6].contains(section) {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TitleCell") as! TitleCell
+            
+            switch section {
+                case 0:
+                    cell.titleLabel.text = PandaExpress.Plate.title
+                case 2:
+                    cell.titleLabel.text = PandaExpress.BiggerPlate.title
+                case 4:
+                    cell.titleLabel.text = PandaExpress.Bowl.title
+                case 6:
+                    cell.titleLabel.text = PandaExpress.FamilyFeast.title
+                default:
+                    cell.titleLabel.text = "Error"
+            }
+            
+            return cell
+            
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "ItemDescriptionCell") as! ItemDescriptionCell
+            
+            switch section {
+                case 1:
+                    cell.headerLabel.text = PandaExpress.Plate.header[row]
+                    cell.descriptionLabel.text = PandaExpress.Plate.description[row]
+                    cell.priceLabel.text = "$\(PandaExpress.Plate.price)"
+                case 3:
+                    cell.headerLabel.text = PandaExpress.BiggerPlate.header[row]
+                    cell.descriptionLabel.text = PandaExpress.BiggerPlate.description[row]
+                    cell.priceLabel.text = "$\(PandaExpress.BiggerPlate.price)"
+                case 5:
+                    cell.headerLabel.text = PandaExpress.Bowl.header[row]
+                    cell.descriptionLabel.text = PandaExpress.Bowl.description[row]
+                    cell.priceLabel.text = "$\(PandaExpress.Bowl.price)"
+                case 7:
+                    cell.headerLabel.text = PandaExpress.FamilyFeast.header[row]
+                    cell.descriptionLabel.text = PandaExpress.FamilyFeast.description[row]
+                    cell.priceLabel.text = "$\(PandaExpress.FamilyFeast.price)"
+                default:
+                    cell.headerLabel.text = "Error"
+                
+            }
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
 
 }
