a375c6ca8f99c74ff524e812e8c994fb1b8f2c43
Chinenye Ogbuchiekwe <ogbuchic@uci.edu>
2019-08-28 11:02:13 -0700

    Added pageControl and pageIndicator

--- Roamer/Storyboards/MainViewController.swift
+++ Roamer/Storyboards/MainViewController.swift
@@ -1,77 +1,77 @@
+////
+////  MainViewController.swift
+////  Roam
+////
+////  Created by chinenye ogbuchiekwe on 8/13/19.
+////  Copyright © 2019 Darrel Muonekwu. All rights reserved.
+////
 //
-//  MainViewController.swift
-//  Roam
+//import UIKit
 //
-//  Created by chinenye ogbuchiekwe on 8/13/19.
-//  Copyright © 2019 Darrel Muonekwu. All rights reserved.
-//
-
-import UIKit
-
-class MainViewController: UIViewController {
-    
-    @IBOutlet weak var scrollView: UIScrollView!
-    
-    override func viewDidLoad() {
-        
-        let V1 = self.storyboard?.instantiateViewController(withIdentifier: "V1") as UIViewController?
-        
-        self.addChild(V1!)
-        self.scrollView.addSubview(V1!.view)
-        V1?.didMove(toParent: self)
-        V1?.view.frame = scrollView.bounds
-        
-        
-        
-        let V2 = self.storyboard?.instantiateViewController(withIdentifier: "V2") as UIViewController?
-        
-        self.addChild(V2!)
-        self.scrollView.addSubview(V2!.view)
-        V2?.didMove(toParent: self)
-        V2?.view.frame = scrollView.bounds
-        
-        var V2Frame: CGRect = (V2?.view.frame)!
-        V2Frame.origin.x = self.view.frame.width
-        V2?.view.frame = V2Frame
-        
-        
-        
-        let V3 = self.storyboard?.instantiateViewController(withIdentifier: "V3") as UIViewController?
-        
-        self.addChild(V3!)
-        self.scrollView.addSubview(V3!.view)
-        V3?.didMove(toParent: self)
-        V3?.view.frame = scrollView.bounds
-        
-        
-        var V3Frame: CGRect = (V3?.view.frame)!
-        V3Frame.origin.x = 2 * self.view.frame.width
-        V3?.view.frame = V3Frame
-        
-        //        if scrollView.tag == 10 {
-        //
-        //            self.scrollView.contentSize = CGSize( width: (self.view.frame.width) * 3, height: (self.view.frame.height))
-        //        }
-        self.scrollView.contentSize = CGSize( width: (self.view.frame.width) * 3, height: (self.view.frame.height))
-        
-        
-        self.scrollView.contentOffset = CGPoint(x: (self.view.frame.width) * 1, y: (self.view.frame.height))
-        
-        super.viewDidLoad()
-        
-        
-        // Do any additional setup after loading the view.
-    }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-}
+//class MainViewController: UIViewController {
+//    
+//    @IBOutlet weak var scrollView: UIScrollView!
+//    
+//    override func viewDidLoad() {
+//        
+//        let V1 = self.storyboard?.instantiateViewController(withIdentifier: "V1") as UIViewController?
+//        
+//        self.addChild(V1!)
+//        self.scrollView.addSubview(V1!.view)
+//        V1?.didMove(toParent: self)
+//        V1?.view.frame = scrollView.bounds
+//        
+//        
+//        
+//        let V2 = self.storyboard?.instantiateViewController(withIdentifier: "V2") as UIViewController?
+//        
+//        self.addChild(V2!)
+//        self.scrollView.addSubview(V2!.view)
+//        V2?.didMove(toParent: self)
+//        V2?.view.frame = scrollView.bounds
+//        
+//        var V2Frame: CGRect = (V2?.view.frame)!
+//        V2Frame.origin.x = self.view.frame.width
+//        V2?.view.frame = V2Frame
+//        
+//        
+//        
+//        let V3 = self.storyboard?.instantiateViewController(withIdentifier: "V3") as UIViewController?
+//        
+//        self.addChild(V3!)
+//        self.scrollView.addSubview(V3!.view)
+//        V3?.didMove(toParent: self)
+//        V3?.view.frame = scrollView.bounds
+//        
+//        
+//        var V3Frame: CGRect = (V3?.view.frame)!
+//        V3Frame.origin.x = 2 * self.view.frame.width
+//        V3?.view.frame = V3Frame
+//        
+//        //        if scrollView.tag == 10 {
+//        //
+//        //            self.scrollView.contentSize = CGSize( width: (self.view.frame.width) * 3, height: (self.view.frame.height))
+//        //        }
+//        self.scrollView.contentSize = CGSize( width: (self.view.frame.width) * 3, height: (self.view.frame.height))
+//        
+//        
+//        self.scrollView.contentOffset = CGPoint(x: (self.view.frame.width) * 1, y: (self.view.frame.height))
+//        
+//        super.viewDidLoad()
+//        
+//        
+//        // Do any additional setup after loading the view.
+//    }
+//    
+//    
+//    /*
+//     // MARK: - Navigation
+//     
+//     // In a storyboard-based application, you will often want to do a little preparation before navigation
+//     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//     // Get the new view controller using segue.destination.
+//     // Pass the selected object to the new view controller.
+//     }
+//     */
+//    
+//}
---
+++ Roamer/Storyboards/PageViewController.swift
@@ -0,0 +1,101 @@
+//
+//  PageViewController.swift
+//  Roam
+//
+//  Created by chinenye ogbuchiekwe on 8/26/19.
+//  Copyright © 2019 Darrel Muonekwu. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+    
+    lazy var orderedViewControllers : [UIViewController] = { return [self.newVc(viewController: "V1"), self.newVc(viewController: "V2"), self.newVc(viewController: "V3") ]}()
+    
+    var pageControl = UIPageControl()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        if  let firstViewController = orderedViewControllers.first{
+            setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+            
+            self.delegate = self
+            configurePageControl()
+        }
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    func configurePageControl(){
+        pageControl = UIPageControl(frame: CGRect(x:0 , y :UIScreen.main.bounds.maxY - 50,  width: UIScreen.main.bounds.width, height:50))
+        pageControl.numberOfPages = orderedViewControllers.count
+        pageControl.currentPage = 0
+        pageControl.tintColor = UIColor.black
+        pageControl.pageIndicatorTintColor = UIColor.gray
+        pageControl.currentPageIndicatorTintColor = UIColor.blue
+        self.view.addSubview(pageControl)
+    }
+    
+    func newVc(viewController: String) -> UIViewController{
+        return UIStoryboard(name: "Roamer", bundle: nil).instantiateViewController(withIdentifier: viewController)
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.firstIndex(of:  viewController) else{
+            return nil
+            
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else{
+            
+            return orderedViewControllers.last
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+        
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.firstIndex(of:  viewController) else{
+            return nil
+            
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard orderedViewControllers.count != nextIndex else{
+            
+            return orderedViewControllers.first
+        }
+        
+        guard orderedViewControllers.count  > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = orderedViewControllers.firstIndex(of : pageContentViewController)!
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+}
